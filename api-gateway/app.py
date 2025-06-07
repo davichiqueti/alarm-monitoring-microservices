9942a848c0d27dd37b256dbba08db99975f1fda1
Davi Chiqueti <davichiqueti@gmail.com>
2025-06-07 16:15:02 -0300

    fix gateway handling

--- api-gateway/app.py
+++ api-gateway/app.py
@@ -4,32 +4,35 @@ import httpx
 
 
 app = FastAPI()
-services = {
+ALLOWED_METHODS = ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"]
+SERVICES = {
     "users-app": "http://users-app:8000",
 }
 
 
 async def forward_request(service_url: str, method: str, path: str, body=None, headers=None):
+    # Avoiding redirect errors on URLs with no "/" on end
     if not path.endswith("/"):
         path += "/"
     url = f"{service_url}{path}?format=json"
+    if headers:
+        # Letting content metadata definition be auto set to avoid errors
+        headers.pop("content-length", None)
+        headers.pop("content-type", None)
+    # Sending request
     async with httpx.AsyncClient() as client:
-        response = await client.request(
-            method,
-            url,
-            json=body,
-            headers=headers,
-        )
-        return response
+        return await client.request(method, url, json=body, headers=headers)
 
 
-@app.api_route("/{service}/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
+@app.api_route("/{service}/{path:path}", methods=ALLOWED_METHODS)
 async def gateway(service: str, path: str, request: Request):
-    if service not in services:
+    if service not in SERVICES:
         raise HTTPException(status_code=404, detail="Service not found")
 
-    service_url = services[service]
-    body = await request.json() if request.method in ["POST", "PUT", "PATCH"] else None
+    service_url = SERVICES[service]
+    body = None
+    if request.method in ["POST", "PUT", "PATCH"]:
+        body = await request.json()
     headers = dict(request.headers)
     response = await forward_request(service_url, request.method, f"/{path}", body, headers)
 
@@ -38,4 +41,4 @@ async def gateway(service: str, path: str, request: Request):
     except Exception:
         content = {"detail": response.text}
 
-    return JSONResponse(status_code=response.status_code, content=content)
+    return JSONResponse(content, response.status_code)
